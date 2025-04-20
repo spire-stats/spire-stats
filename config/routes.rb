bd5e4bcedae743f18af0deebc04cf677d98a2d08
Kagen <kl.hearn40@gmail.com>
2025-04-20 16:23:56 -0700

    Add admin flag and pghero route

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "run_file#index"
 
-  if Current.user&.admin?
+  authenticate :user, ->(user) { user.admin? } do
     mount PgHero::Engine, at: "pghero"
   end
 end
