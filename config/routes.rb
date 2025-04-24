9e474c8e472eaa9ba6d415311e2f60be161e79f1
Kagen <kl.hearn40@gmail.com>
2025-04-23 19:45:28 -0700

    Merge from main

--- config/routes.rb
+++ config/routes.rb
@@ -15,4 +15,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "run_file#index"
+
+  resources :runs, only: [ :index, :show ]
+
+  authenticate :user, ->(user) { user.admin? } do
+    mount PgHero::Engine, at: "pghero"
+  end
 end
