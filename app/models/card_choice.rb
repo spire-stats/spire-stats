8d88aeb6a712d6d8fcc86f615fea232973d46773
Kagen <kl.hearn40@gmail.com>
2025-06-01 18:51:30 -0700

    Simplify card choice

--- app/models/card_choice.rb
+++ app/models/card_choice.rb
@@ -10,23 +10,19 @@ class CardChoice < ApplicationRecord
   scope :mid_game, -> { where(floor: 18..34) }
   scope :late_game, -> { where(floor: 35..Float::INFINITY) }
 
-  # Method to process card choices from run file
   def self.process_from_run_data(run, card_choices)
-    return [] unless card_choices
-
-    results = []
+    return unless card_choices
 
     card_choices.each do |choice|
       floor = choice["floor"]
-      context = choice["context"] # might be nil
+      context = choice["context"]
 
-      # Process the picked card
       if choice["picked"] != "SKIP"
         base_name = choice["picked"].gsub(/\+\d+$/, "")
         card = Card.find_by(name: base_name)
 
         if card
-          results << create(
+          create(
             run: run,
             card: card,
             floor: floor,
@@ -36,14 +32,13 @@ class CardChoice < ApplicationRecord
         end
       end
 
-      # Process the not picked cards
       if choice["not_picked"].present?
         choice["not_picked"].each do |not_picked_name|
           base_name = not_picked_name.gsub(/\+\d+$/, "")
           card = Card.find_by(name: base_name)
 
           if card
-            results << create(
+            create(
               run: run,
               card: card,
               floor: floor,
@@ -54,7 +49,5 @@ class CardChoice < ApplicationRecord
         end
       end
     end
-
-    results
   end
 end
